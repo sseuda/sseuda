9326d0e54512c7480a4e7b9b2eea6c5ab0f71335
jieunjieune <jieune120@gmail.com>
2025-04-02 18:14:07 +0900

    초기 커밋

---
+++ sseuda-react/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>메에롱</p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ sseuda_server/src/main/java/com/sseuda/sseuda_server/SseudaServerApplication.java
@@ -0,0 +1,13 @@
+package com.sseuda.sseuda_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseudaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseudaServerApplication.class, args);
+    }
+
+}
