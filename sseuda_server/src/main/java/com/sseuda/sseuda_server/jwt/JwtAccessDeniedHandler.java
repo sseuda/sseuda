9611c40c04dfddd97ee33daa8e049db68cf6ca9d
jieunjieune <jieune120@gmail.com>
2025-04-27 19:03:46 +0900

    feat: 토큰인증 관련(jwt)

--- sseuda_server/src/main/java/com/sseuda/sseuda_server/jwt/JwtAccessDeniedHandler.java
+++ sseuda_server/src/main/java/com/sseuda/sseuda_server/jwt/JwtAccessDeniedHandler.java
@@ -1,4 +1,17 @@
 package com.sseuda.sseuda_server.jwt;
 
-public class JwtAccessDeniedHandler {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    public void handle(HttpServletRequest req, HttpServletResponse res,
+                       AccessDeniedException e) throws IOException, ServletException {
+        res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
 }
