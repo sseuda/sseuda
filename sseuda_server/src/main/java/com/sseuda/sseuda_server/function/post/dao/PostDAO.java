794c7fa1a5420fa90f35b67b2834f7ad42f4cdb9
soyuul <syuul3139@gmail.com>
2025-04-27 16:40:31 +0900

    feat: post controller, service, dao 어노테이션/ dto 삽입

--- sseuda_server/src/main/java/com/sseuda/sseuda_server/function/post/dao/PostDAO.java
+++ sseuda_server/src/main/java/com/sseuda/sseuda_server/function/post/dao/PostDAO.java
@@ -1,4 +1,7 @@
 package com.sseuda.sseuda_server.function.post.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface PostDAO {
 }
--- sseuda_server/src/main/java/com/sseuda/sseuda_server/function/post/service/PostService.java
+++ sseuda_server/src/main/java/com/sseuda/sseuda_server/function/post/service/PostService.java
@@ -1,4 +1,7 @@
 package com.sseuda.sseuda_server.function.post.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PostService {
 }
