ccc0b0ce31bafc537f579cd5bcc42627fefd774f
jieunjieune <jieune120@gmail.com>
2025-04-27 19:03:46 +0900

    feat: 토큰인증 관련(jwt)

--- sseuda_server/src/main/java/com/sseuda/sseuda_server/jwt/TokenDTO.java
+++ sseuda_server/src/main/java/com/sseuda/sseuda_server/jwt/TokenDTO.java
@@ -1,4 +1,49 @@
 package com.sseuda.sseuda_server.jwt;
 
 public class TokenDTO {
+
+    private String grantType;
+    private String username;
+    private String accessToken;
+    private Long accessTokenExpiresIn; // 액세스 토큰 만료 시간
+
+    public TokenDTO(String grantType, String memberName, String accessToken, Long accessTokenExpiresIn) {
+        this.grantType = grantType;
+        this.username = memberName;
+        this.accessToken = accessToken;
+        this.accessTokenExpiresIn = accessTokenExpiresIn;
+    }
+
+    // Getter와 Setter 추가 (Lombok을 사용할 수도 있음)
+    public String getGrantType() {
+        return grantType;
+    }
+
+    public void setGrantType(String grantType) {
+        this.grantType = grantType;
+    }
+
+    public String getMemberName() {
+        return username;
+    }
+
+    public void setMemberName(String memberName) {
+        this.username = username;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public Long getAccessTokenExpiresIn() {
+        return accessTokenExpiresIn;
+    }
+
+    public void setAccessTokenExpiresIn(Long accessTokenExpiresIn) {
+        this.accessTokenExpiresIn = accessTokenExpiresIn;
+    }
 }
--- sseuda_server/src/main/java/com/sseuda/sseuda_server/jwt/TokenProvider.java
+++ sseuda_server/src/main/java/com/sseuda/sseuda_server/jwt/TokenProvider.java
@@ -1,4 +1,11 @@
 package com.sseuda.sseuda_server.jwt;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TokenProvider {
+
 }
+
