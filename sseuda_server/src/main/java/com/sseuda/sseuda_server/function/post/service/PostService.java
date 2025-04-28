c85a189e20e58ddce60e9d415bd09fa89836488f
soyul <syuul3139@gmail.com>
2025-04-28 15:12:20 +0900

    feat: 게시글 전체 조회

--- sseuda_server/src/main/java/com/sseuda/sseuda_server/function/post/service/PostService.java
+++ sseuda_server/src/main/java/com/sseuda/sseuda_server/function/post/service/PostService.java
@@ -1,7 +1,28 @@
 package com.sseuda.sseuda_server.function.post.service;
 
+import com.sseuda.sseuda_server.function.post.controller.PostController;
+import com.sseuda.sseuda_server.function.post.dao.PostMapper;
+import com.sseuda.sseuda_server.function.post.dto.PostDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
+
+    private static final Logger log = LoggerFactory.getLogger(PostController.class);
+    private final PostMapper postMapper;
+
+    @Autowired
+    public PostService(PostMapper postMapper) {
+        this.postMapper = postMapper;
+    }
+
+    public List<PostDTO> findPostList(){
+
+        return postMapper.findPostList();
+    }
 }
