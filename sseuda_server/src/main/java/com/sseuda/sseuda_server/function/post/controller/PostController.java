04a5cc95de26345ab95c346a2e6b2739c62a7cb1
soyul <syuul3139@gmail.com>
2025-04-28 15:12:20 +0900

    feat: 게시글 전체 조회

--- sseuda_server/src/main/java/com/sseuda/sseuda_server/function/post/controller/PostController.java
+++ sseuda_server/src/main/java/com/sseuda/sseuda_server/function/post/controller/PostController.java
@@ -1,7 +1,33 @@
 package com.sseuda.sseuda_server.function.post.controller;
 
-import org.springframework.stereotype.Controller;
+import com.sseuda.sseuda_server.common.ResponseDTO;
+import com.sseuda.sseuda_server.function.post.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/*")
 public class PostController {
+
+    private static final Logger log = LoggerFactory.getLogger(PostController.class);
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @Operation(summary = "포스팅 전체 조회", description = "포스팅 전체 조회", tags = {"PostController"})
+    @GetMapping("/mypage")
+    public ResponseEntity<ResponseDTO> findPostList(){
+
+        return ResponseEntity.ok(new ResponseDTO(HttpStatus.OK, "조회 성공", postService.findPostList()));
+    }
 }
--- sseuda_server/src/main/java/com/sseuda/sseuda_server/function/post/dao/PostMapper.java
+++ sseuda_server/src/main/java/com/sseuda/sseuda_server/function/post/dao/PostMapper.java
@@ -1,4 +1,12 @@
 package com.sseuda.sseuda_server.function.post.dao;
 
+import com.sseuda.sseuda_server.function.post.dto.PostDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface PostMapper {
+
+    List<PostDTO> findPostList();
 }
