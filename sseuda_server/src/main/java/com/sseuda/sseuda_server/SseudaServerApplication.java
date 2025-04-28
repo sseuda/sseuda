ffd9ad1bdd4efe560e0f5e03d09f82bb159842d6
soyul <syuul3139@gmail.com>
2025-04-28 12:29:02 +0900

    초기 파일 수정

--- sseuda_server/src/main/java/com/sseuda/sseuda_server/SseudaServerApplication.java
+++ sseuda_server/src/main/java/com/sseuda/sseuda_server/SseudaServerApplication.java
@@ -1,9 +1,12 @@
 package com.sseuda.sseuda_server;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.sseuda.sseuda_server", annotationClass = Mapper.class)
 public class SseudaServerApplication {
 
     public static void main(String[] args) {
