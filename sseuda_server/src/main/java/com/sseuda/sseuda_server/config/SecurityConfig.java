eeb1c13ede86a8d3528c31c4d69746a07e8e72c9
soyul <syuul3139@gmail.com>
2025-04-30 16:45:49 +0900

    fix: category_big merge 오류

--- sseuda_server/src/main/java/com/sseuda/sseuda_server/config/SecurityConfig.java
+++ sseuda_server/src/main/java/com/sseuda/sseuda_server/config/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.sseuda.sseuda_server.config;
 
+import com.sseuda.sseuda_server.member.exception.AuthFailHandler;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
