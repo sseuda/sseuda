0e1fa8f44401bfd49d453d126b0c3e136c8dcede
soyul <syuul3139@gmail.com>
2025-05-16 15:48:33 +0900

    feat: 게시글 DB에 저장 axios, json에 proxy 추가

--- sseuda-react/src/components/common/TextEditor.js
+++ sseuda-react/src/components/common/TextEditor.js
@@ -1,20 +1,41 @@
+// HTTP 요청 (POST/GET/DELETE/PUT) 을 위해 사용용
+import axios from 'axios';
 import React, { useState } from 'react'
 import ReactQuill from 'react-quill'
+// Quill 에디터 css (필수)
 import "react-quill/dist/quill.snow.css";
+
 
 function TextEditor() {
 
+    // quill의 내용을 저장할 상태 변수
     const [content, setContent] = useState("");
 
+    // 상태(텍스트)가 변할때마다 상태를 업데이트하는 함수
     const handleChange = value => {
         setContent(value);
     }
 
+    const handleSave = async () => {
+        
+        try{
+            const response = await axios.post("/post/posts", {
+                content: content    // Quill의 HTML 내용
+            });
+            console.log("저장 성공", response.data);
+        }catch(err){
+            console.log("저장 실패", err)
+        }
+    }
+
   return (
     <>
+        {/* content = 현재 , onChange = 변하는 값(콜백) */}
         <ReactQuill value={content} onChange={handleChange}/>
         <div>
+            <button onClick={handleSave}>저장</button>
             <h3>Preview:</h3>
+            {/* handleChange에 따라 변하는 값을 보여준다 */}
             <div dangerouslySetInnerHTML={{ __html: content}}/>
         </div>
     </>
