2c495ff5d78f97b7556fc346268556229517c80c
soyuul <syuul3139@gmail.com>
2025-05-18 18:34:58 +0900

    마이페이지 제작중

--- sseuda-react/src/pages/myPage/MyPage.js
+++ sseuda-react/src/pages/myPage/MyPage.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 function MyPage() {
+  const dispatch = useDispatch();
+  const {category, postId} = useParams();
+
+  console.log("마이페이지 게시글 리스트 시작");
+
   return (
     <div>
       마이페이지입니다~~
