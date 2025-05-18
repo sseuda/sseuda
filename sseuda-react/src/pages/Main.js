aa64dcf42c4f74259150d1191dea3ba262c6f95e
soyuul <syuul3139@gmail.com>
2025-05-18 17:07:38 +0900

    마이페이지 제작

--- sseuda-react/src/pages/Main.js
+++ sseuda-react/src/pages/Main.js
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 
 
 export default function Main() {
     return (
-        <div>
-			인생은 쓰다...
-        </div>
+        <>
+            <Link to={"/mypage"}>마이페이지 바로가기</Link>
+        </>
     );
 }
