be1f4172788a4192486cae3bc021944a56c2df68
soyuul <syuul3139@gmail.com>
2025-05-21 10:59:25 +0900

    메인 페이지 게시글 부분 자리잡기

--- sseuda-react/src/pages/Main.js
+++ sseuda-react/src/pages/Main.js
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import PostMain from "./post/PostMain";
+import MainCSS from "./Main.module.css";
 
 function Main() {
 
     
   return (
     <>
-      <PostMain/>
-      <Link to={"/mypage"}>마이페이지 바로가기</Link>
+      <div className={MainCSS.mainBox}>
+        <PostMain/>
+        <Link to={"/mypage"}>마이페이지 바로가기</Link>
+      </div>
     </>
   )
 }
