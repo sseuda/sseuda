1ae744a5de4835ef5bbf7f5c8cf28988128ab00b
soyuul <syuul3139@gmail.com>
2025-05-23 17:11:44 +0900

    마이페이지 네비바 css

--- sseuda-react/src/layouts/MypageLayout.js
+++ sseuda-react/src/layouts/MypageLayout.js
@@ -4,16 +4,17 @@ import TextEditor from '../components/common/post/TextEditor';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
+import MypageNavbar from '../components/common/MypageNavbar';
 
 function MypageLayout() {
   return (
-    <div>
+    <div style={{position: 'relative'}}>
       <Header/>
-      <main>
+      <div style={{display: 'flex'}}>
+        <MypageNavbar style={{position: 'absolute', top: '0', left: '0'}}/>
         {/* <TextEditor/> */}
         <Outlet/>
-      </main>
-      <Footer/>
+      </div>
     </div>
   )
 }
