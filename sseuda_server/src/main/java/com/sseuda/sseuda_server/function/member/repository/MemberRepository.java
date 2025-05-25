635dc45d1d0dab7f6fb3a98ef226201ee73958eb
soyuul <syuul3139@gmail.com>
2025-05-25 16:28:50 +0900

    없어져라

--- sseuda_server/src/main/java/com/sseuda/sseuda_server/function/member/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sseuda.sseuda_server.function.member. repository;
-
-import com.sseuda.sseuda_server.function.member.pojo.Login;
-
-public interface MemberRepository {
-
-    Login findByUsername(String username);
-}
