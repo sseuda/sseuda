b175dd921de381f3bdc95ee5a9dc95b6abc3410c
jieunjieune <jieune120@gmail.com>
2025-06-07 01:18:03 +0900

    feat: 메인페이지 검색창 추가

--- sseuda-react/src/pages/Main.js
+++ sseuda-react/src/pages/Main.js
@@ -1,9 +1,11 @@
-import { Link, replace, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PostMain from "./post/PostMain";
 import MainCSS from "./Main.module.css";
 import PostBanner from "./post/PostBanner";
-
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { decodeJwt } from "../utils/tokenUtils";
+import { useState } from "react";
 
 function Main() {
   const navigate = useNavigate();
@@ -11,6 +13,16 @@ function Main() {
   const token = localStorage.getItem("accessToken");
   const decoded = token ? decodeJwt(token) : null;
   const username = decoded ? decoded.sub : null;
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // const handleSearch = () => {
+  //   if (searchTerm.trim() === "") {
+  //     alert("검색어를 입력해주세요.");
+  //     return;
+  //   }
+  //   navigate(`/search?query=${encodeURIComponent(searchTerm)}`);
+  // };
 
   const userMyPageList = () => {
     if (username) {
@@ -25,18 +37,37 @@ function Main() {
     
   return (
     <>
-      <PostBanner/>
+      <div className={MainCSS.searchBox}>
+        <div className={MainCSS.searchWrapper}>
+          <input
+            type="text"
+            className={MainCSS.searchInput}
+            placeholder="검색어를 입력하세요"
+            // value={searchTerm}
+            // onChange={(e) => setSearchTerm(e.target.value)}
+            // onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+          />
+          <button className={MainCSS.searchBtn}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} className={MainCSS.searchIcon} />
+          </button>
+        </div>
+      </div>
+
+      <PostBanner />
+
       <div className={MainCSS.mainBox}>
-        <PostMain/>
+        <PostMain />
         <div className={MainCSS.loginBox}>
           <p><b>쓰다</b>에 로그인하여 더 많은 기능을 이용해보세요 :-)</p>
-          <Link to="/auth/login" className={MainCSS.loginBTN}><img src="/images/main/sseudaKorean.png" className={MainCSS.logo}/>로그인</Link>
+          <Link to="/auth/login" className={MainCSS.loginBTN}>
+            <img src="/images/main/sseudaKorean.png" className={MainCSS.logo}/>로그인
+          </Link>
           <div className="bottom-links">
-					<Link to="/member/find-username">아이디 찾기</Link>
-					<span>|</span>
-					<Link to="/member/reset-password-request">비밀번호 찾기</Link>
-					<span>|</span>
-					<Link to="/member/signup">회원가입</Link>
+            <Link to="/member/find-username">아이디 찾기</Link>
+            <span>|</span>
+            <Link to="/member/reset-password-request">비밀번호 찾기</Link>
+            <span>|</span>
+            <Link to="/member/signup">회원가입</Link>
           </div>
           <div onClick={userMyPageList}>마이페이지 바로가기</div>
         </div>
