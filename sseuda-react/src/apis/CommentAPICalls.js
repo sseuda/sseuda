d94c71a1455db9ccbb55a8037f1206f41d19a37a
soyuul <syuul3139@gmail.com>
2025-06-13 12:05:18 +0900

    feat: comment API, module 추가

---
+++ sseuda-react/src/apis/CommentAPICalls.js
@@ -0,0 +1,105 @@
+import { DELETE_USER_COMMENT, GET_COMMENTS, POST_USER_COMMENT, PUT_USER_COMMENT } from "../modules/CommentModule";
+
+const prefix = `http://${process.env.REACT_APP_RESTAPI_IP}:8080`;
+
+// 게시글별 댓글 전체 조회
+export const callPostCommentListApi = () =>{
+
+    let requestURL = `${prefix}/post/comment`;
+    console.log('[CommentAPICalls] requestURL : ', requestURL);
+
+    return async (dispatch, getState) =>{
+        const result = await fetch(requestURL, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: '*/*'
+            }
+        }).then((response) => response.json());
+        if(result.status === 200){
+            console.log('[CommentAPICalls] callPostCommentListApi RESULT : ', result);
+            dispatch({type:GET_COMMENTS, payload: result.data});
+        }
+    };
+};
+
+
+// 게시글별 특정 회원 댓글 등록
+export const callPostCommentRegistApi = ({form, username}) =>{
+    console.log('[CommentAPICalls] callPostCommentRegistApi');
+
+    const requestURL = `${prefix}/post/comment/${username}/input`;
+
+    return async(dispatch, getState) =>{
+        const result = await fetch(requestURL,{
+            method: 'POST',
+            headers: {
+                Accept: '*/*',
+                Authorization:
+                    'Bearer ' + window.localStorage.getItem('accessToken')
+            },
+            body: form
+        }).then((response) => response.json());
+
+        console.log('[CommentAPICalls] callPostCommentRegistApi RESULT : ', result);
+        dispatch({type: POST_USER_COMMENT, payload: result});
+    };
+};
+
+
+// 게시글별 특정 회원 댓글 수정
+export const callPostCommentUpdateApi = ({ form, username }) =>{
+    console.log('[CommentAPICalls] callPostCommentUpdateApi');
+
+    for(let [key, value] of form.entries()){
+        console.log(`??${key}: ${value}`);
+    }
+
+    const requestURL = `${prefix}/post/comment/${username}/update`;
+    console.log("requestURL: ", requestURL);
+
+    return async(dispatch, getState) =>{
+        const result = await fetch(requestURL, {
+            method: "PUT",
+            headers: {
+                Accept: '*/*',
+                Authorization:
+                    'Bearer ' + window.localStorage.getItem("accessToken")
+            },
+            body: form,
+        }).then((response) => response.json())
+        .catch((error) =>{
+            console.error("[CommentAPICalls] Error: ", error);
+            throw error;
+        });
+
+        if(result.status === 200){
+            console.log("[CommentAPICalls] callPostCommentUpdateApi RESULT: ", result);
+            dispatch({type: PUT_USER_COMMENT, payload: result});
+        }
+    };
+};
+
+
+// 게시글별 특정 회원 댓글 삭제
+export const callPostCommentDeleteApi = ({form, username}) =>{
+    console.log('[CommentAPICalls] callPostCommentDeleteApi');
+
+    const requestURL = `${prefix}/post/comment/${username}/delete`;
+
+    return async(dispatch, getState) =>{
+        const result = await fetch(requestURL, {
+            method: 'DELETE',
+            headers: {
+                Accept: '*/*',
+                Authorization:
+                    'Bearer: ' + window.localStorage.getItem("accessToken")
+            },
+            body: form
+        }).then((response) => response.json());
+
+        console.log('[CommentAPICalls] callPostCommentDeleteApi RESULT: ', result);
+
+        dispatch({type: DELETE_USER_COMMENT, payload: result});
+    };
+};
