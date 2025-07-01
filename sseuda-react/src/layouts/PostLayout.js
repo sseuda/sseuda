1a3be48492433eae2a62bd246d7cb5ed6f303528
soyuul <syuul3139@gmail.com>
2025-07-01 11:12:05 +0900

    fix: 내 게시물 전체보기 카테고리 리스트 수정

--- sseuda-react/src/layouts/PostLayout.js
+++ sseuda-react/src/layouts/PostLayout.js
@@ -2,14 +2,12 @@ import React from 'react'
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
 import { Outlet } from 'react-router-dom';
-import CategoryNav from '../components/common/category/CategoryNav';
 
 function PostLayout() {
   return (
     <div style={{position: 'relative'}}>
 		<Header />
 		<div>
-			{/* <CategoryNav style={{position: 'absolute', top: '0', left: '0'}}/> */}
 			<div>
 				<Outlet/>
 			</div>
